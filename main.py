08aabd680c4acdeee00f16176dee4abbff880f97
Varyamo <zalygin.04@gmail.com>
2022-02-10 13:12:15 +0800

    add function encryption word with MD5

---
+++ main.py
@@ -0,0 +1,14 @@
+import hashlib
+
+def encrypt_with_md5(word_to_encrypt):
+    encrypted_word = hashlib.md5(word_to_encrypt.encode('utf-8'))
+    return encrypted_word.hexdigest()
+
+group_of_encryption_methods = {
+    "MD5": encrypt_with_md5,
+}
+
+word_to_encrypt = input('Введите слово или фразу для шифрования:\n')
+user_encryption_method = input('Каким способом зашифровать:\n1. MD5\n2. SHA1\n> ')
+
+print(group_of_encryption_methods[user_encryption_method](word_to_encrypt))
