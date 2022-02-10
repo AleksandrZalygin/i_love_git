cce05839af7c68b938da9f8d584d06ba8f00f5d8
Varyamo <zalygin.04@gmail.com>
2022-02-10 13:17:08 +0800

    add function encryption with SHA1

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ def encrypt_with_md5(word_to_encrypt):
     encrypted_word = hashlib.md5(word_to_encrypt.encode('utf-8'))
     return encrypted_word.hexdigest()
 
+def encrypt_with_sha1(word_to_encrypt):
+    encrypted_word = hashlib.sha1(word_to_encrypt.encode('utf-8'))
+    return encrypted_word.hexdigest()
+
 group_of_encryption_methods = {
     "MD5": encrypt_with_md5,
+    "SHA1": encrypt_with_sha1,
 }
 
 word_to_encrypt = input('Введите слово или фразу для шифрования:\n')
